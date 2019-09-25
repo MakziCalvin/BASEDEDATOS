91b32fbb68757851c1bd287f5e2b01a4b639e6c4
matzi <maximiliano.calvin1@gmail.com>
2019-09-25 16:23:47 -0300

    segundo-repositorio

---
+++ JavaLibrary1/src/entidades/BaseDeDatos.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidades;
+
+/**
+ *
+ * @author Matzi
+ */
+public class BaseDeDatos {
+    private String id;
+    private Object data;
+
+    public BaseDeDatos(String id, Object data) {
+        this.id = id;
+        this.data = data;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+    
+    
+}
